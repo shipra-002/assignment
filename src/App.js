5a2d1056e6b86814cf5ea836646f6de9b5d9298d
shipra-002 <shipraarora518@gmail.com>
2024-05-23 14:18:47 +0530

    notes-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import CreateNote from './components/createNotes';
+import GetNotes from './components/getNotes';
+import UpdateNote from './components/updateNotes';
+import DeleteNote from './components/deleteNotes';
+import './App.css'; // Import the CSS file
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <h1>
+          <Link to="/createNotes">
+            Notes App
+          </Link>
+        </h1>
+        <nav>
+          <ul>
+            <li><Link to="/createNotes">Create Note</Link></li>
+            <li><Link to="/getNotes">Get Notes</Link></li>
+            <li><Link to="/updateNotes">Update Note</Link></li>
+            <li><Link to="/deleteNotes">Delete Note</Link></li>
+          </ul>
+        </nav>
+        <div className="content">
+          <Routes>
+            {/* <Route path="/" element={<Navigate to="/createNotes" />} /> */}
+            <Route path="/createNotes" element={<CreateNote />} />
+            <Route path="/getNotes" element={<GetNotes />} />
+            <Route path="/updateNotes" element={<UpdateNote />} />
+            <Route path="/deleteNotes" element={<DeleteNote />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
-}
+  <Route path="/" element={<Navigate to="/createNotes" />} />
+
+};
 
 export default App;
+
